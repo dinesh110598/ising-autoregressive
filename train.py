a5976208ab9e8bcd2c02df0c25f09aabc1426db7
Dinesh PR <dineshramakrishnan2012@gmail.com>
2021-02-25 13:55:05 +0530

    Defined trainer class to prevent retracing

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 # %%
 import ising
-import library
 import numpy as np
 from tensorflow import keras as tfk
 from tensorflow import math as tfm
@@ -8,73 +7,79 @@ import tensorflow as tf
 from tqdm import tqdm
 from time import time
 # %%
-lr_schedule = tfk.optimizers.schedules.ExponentialDecay(0.01, 500, 0.8, True)
-optimizer = tfk.optimizers.Adam(lr_schedule, 0.5, 0.999)
-beta_anneal = 0.99
-model = library.PixelCNN()
-batch_size = 50
-sample = tf.Variable(tf.zeros([batch_size,model.L,model.L,1]), trainable=False)
+class Trainer:
+    def __init__(self, model, batch_size=50, learning_rate=0.01):
+        self.lr_schedule = tfk.optimizers.schedules.ExponentialDecay(0.01, 500, 0.8, True)
+        self.optimizer = tfk.optimizers.Adam(self.lr_schedule, 0.5, 0.999)
+        self.beta_anneal = 0.99
+        self.model = model
+        self.batch_size= 50
+        self.sample_graph = tf.Variable(tf.zeros([batch_size, self.model.L, self.model.L, 1]), 
+                                        trainable=False)
 
-def backprop(beta, sample, seed):
-    """Performs backpropagation on the calculated loss function
+    @tf.function
+    def backprop(self, beta, seed):
+        """Performs backpropagation on the calculated loss function
 
-    Args:
-        beta (float): Inverse temperature
+        Args:
+            beta (float): Inverse temperature
 
-    Returns:
-        loss (float): The current loss function for the sampled batch
-    """
-    sample = model.graph_sampler(sample, seed)
-    energy = ising.energy(sample)
-    beta = tf.cast(beta, tf.float32)
-    with tf.GradientTape(True, False) as tape:
-        tape.watch(model.trainable_weights)
-        log_prob = model.log_prob(sample)
-        with tape.stop_recording():
-            loss = (log_prob + beta*energy) / (model.L**2)#type: ignore
-        loss_reinforce = tfm.reduce_mean((loss - tfm.reduce_mean(loss))*log_prob)
-    grads = tape.gradient(loss_reinforce, model.trainable_weights)
-    optimizer.apply_gradients(zip(grads, model.trainable_weights))
-    return loss, energy
+        Returns:
+            loss (float): The current loss function for the sampled batch
+        """
+        self.sample_graph = self.model.graph_sampler(self.sample_graph, seed)
+        energy = ising.energy(self.sample_graph)
+        beta = tf.cast(beta, tf.float32)
+        with tf.GradientTape(True, False) as tape:
+            tape.watch(self.model.trainable_weights)
+            log_prob = self.model.log_prob(self.sample_graph)
+            with tape.stop_recording():
+                loss = (log_prob + beta*energy) / (self.model.L**2)#type: ignore
+            loss_reinforce = tfm.reduce_mean((loss - tfm.reduce_mean(loss))*log_prob)
+        grads = tape.gradient(loss_reinforce, self.model.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
+        print("Tracing")
+        return loss, energy
 
-backprop_graph = tf.function(backprop)#Constructs a graph for faster gradient calculations
+    def train_loop(self, iter, beta, anneal=True):
+        """Runs the unsupervised training loop for VAN training.
 
-def train_loop(iter, beta, anneal=True, **kwargs):
-    """Runs the unsupervised training loop for VAN training.
+        Args:
+            iter (int): No of batches to use for training
+            batch_size (int): No of lattices to sample for single training step
+            beta (float): Inverse temperature to use
+        Options:
+            If model is not None, **kwargs maybe supplied to initialize it.
+            See docstring for ising.PixelCNN() for details.
+        """
+        
+        beta = tf.convert_to_tensor(beta)
+        beta_conv = tf.cast(beta, tf.float32)
+        history = {'step':[],'Free energy mean':[], 'Free energy std':[], 'Energy mean':[], 'Energy std':[],
+        'Train time':[]}
+        interval = 20
 
-    Args:
-        iter (int): No of batches to use for training
-        batch_size (int): No of lattices to sample for single training step
-        beta (float): Inverse temperature to use
-    Options:
-        If model is not None, **kwargs maybe supplied to initialize it.
-        See docstring for ising.PixelCNN() for details.
-    """
-    
-    beta_conv = tf.cast(beta, tf.float32)
-    history = {'step':[],'Free energy mean':[], 'Free energy std':[], 'Energy mean':[], 'Energy std':[],
-    'Train time':[]}
-    interval = 20
+        #Routines required for graph compilation
+        seed_graph = tf.Variable(np.random.randint(-2**30, 2**30, size=2, dtype=np.int32),
+            dtype=tf.int32, trainable=False)
+        t1 = time()
+        
+        for step in tqdm(range(iter)):
+            if anneal==True:
+                beta = beta_conv*(1 - self.beta_anneal**step)
+            if step==0:
+                print(type(beta))
+            loss, energy = self.backprop(beta, seed_graph) #type: ignore
 
-    #Routines required for graph compilation
-    seed_graph = tf.Variable(np.random.randint(-2**30, 2**30, size=2, dtype=np.int32),
-        dtype=tf.int32, trainable=False)
-    t1 = time()
-    
-    for step in tqdm(range(iter)):
-        if anneal==True:
-            beta = beta_conv*(1 - beta_anneal**step)
-        loss, energy = backprop_graph(beta, sample_graph, seed_graph) #type: ignore
-
-        if (step%interval) == interval-1:
-            t2 = time()
-            history['step'].append(step+1)
-            history['Free energy mean'].append( tfm.reduce_mean(loss))
-            history['Free energy std'].append( tfm.reduce_std(loss))
-            history['Energy mean'].append( tfm.reduce_mean(energy))
-            history['Energy std'].append( tfm.reduce_std(energy))
-            history['Train time'].append( (t2-t1)/interval)
-            t1 = time()
-    
-    return history
+            if (step%interval) == interval-1:
+                t2 = time()
+                history['step'].append(step+1)
+                history['Free energy mean'].append( tfm.reduce_mean(loss))
+                history['Free energy std'].append( tfm.reduce_std(loss))
+                history['Energy mean'].append( tfm.reduce_mean(energy))
+                history['Energy std'].append( tfm.reduce_std(energy))
+                history['Train time'].append( (t2-t1)/interval)
+                t1 = time()
+        
+        return history
 # %%
